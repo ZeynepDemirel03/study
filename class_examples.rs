c9f7ce6fc1fc4ea3afe123486aa6dc6f493fa108
Zeynep Demirel <zeynepdmrl1903@gmail.com>
2025-10-29 14:02:17 +0100

    rust study

---
+++ class_examples.rs
@@ -0,0 +1,38 @@
+/*By using the learned concepts, write a function with a proper signature that ...
+ ... calculates the absolute value of a number.
+ ... enforces a number to be in a given range.
+ ... determines the maximum of three numbers.
+ ... determines the amount of days for a given month.
+ ... prints the grade for this course based on four test results.
+ ... prints the body mass index category for a given weight and height.
+ ... calculates the factorial of a given number (advanced) */
+
+/* By using the learned concepts, write a function with a proper signature that ...
+ ... mutates a number by adding another number n.
+ ... concatenates two strings.
+ ... swaps the values of two variables.
+ ... returns a nicely formatted string congratulation for a given birthday.
+ ... repeats a string n times, thus producing a new string (advanced).
+ ... reverses the digits of a positive integer (advanced) */
+
+ /* By using the learned concepts, write a function with a proper signature that ...
+ ... finds the minimum and the sum of a collection of numbers.
+ ... prints the body mass index category (2nd exercise) using pattern matching.
+ ... returns the longest prefix of a collection having numbers smaller than n.
+ ... returns all prime numbers up to a given number n.
+ ... sorts a collection of people with names and ages by name.
+ ... prints a collection of heroes with names and health points in a nice way by
+ visualizing them below each other with their health points as health bars.
+ ... plans the ultimate heist in preparation for GTA VI, picking the best
+ combination of items to put into a bag of capacity k, where each item has
+ some value v and some weight w (advanced) */
+
+ /*By using the learned concepts, write types/functions/methods that ...
+ ... calculate the area and circumference of a graphical object, which can
+ either be a rectangle, a right triangle with sides of equal length, or a circle.
+ ... visualize such a graphical object using print statements.
+ ... stepwise build and manipulate a Sudoku board.
+ ... visualize such a Sudoku board using print statements.
+ ... improve the solutions from last time in terms of readability, reliability, etc.
+ ... solve a Sudoku board (advanced). */
+ 
