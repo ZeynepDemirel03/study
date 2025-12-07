c03988d74e9f0d6f24366da4c0957866923deb8f
Zeynep Demirel <zeynepdmrl1903@gmail.com>
2025-12-07 17:33:35 +0100

    Update test_solutions.rs

--- test_solutions.rs
+++ test_solutions.rs
@@ -5,12 +5,16 @@
  where the leftmost element becomes the last element.
  Write a function that converts a String of digits into a number. Tolerate whitespaces at the beginning and the end of the String.
  */
-/*Reminder: Define a custom type that represents a generic collection which can either be an owned vector or an owned array with a size of 10.
+/*Reminder: Define a custom type that represents a generic collection which can either be an owned vector 
+or an owned array with a size of 10.
 Implement your function signature that finds the maximum of a collection defined by your type.
 Implement your function signature which rotates the collection defined by your type n times to the right.
 Implement your function signature that returns all prefixes of a collection defined by your type.
 Write a function that joins the elements of a collection defined by your type into a string using a passed delimiter.
 Implement all of the above as methods for your type instead of functions. */
+
+use std::fmt::Display;
+
 
 fn main(){
     let liste = [1,2,3,4,5];
@@ -19,18 +23,46 @@ fn main(){
 
     println!("{:?}",sum(&liste));
 
-    println!("{:?}", prefix(&liste));
+    println!("{:?}", prefix1(&liste));
 
     let dizi = ["a","b","c","d"];
 
     println!("{:?}", rotates(&dizi,1));
 
-    println!("{}", converts("".to_string()))
+    println!("{}", converts("".to_string()));
+
+    let mut c1 = MyCollection::VecVariant(vec![1,2,3,4,5]);
+    let mut c2 = MyCollection::ArrayVariant([10,20,30,40,50,60,70,80,90,100]);
+    println!("{:?}", c1.find_max());
+    println!("{:?}", c2.find_max());
+
+    c1.rotate_left(1); 
+    println!("{:?}", c1); 
+
+    c2.rotate_left(1);
+    println!("{:?}", c2);
+
+    let prefixes = c1.prefix2(); // veya prefixes()
+
+    for pref in prefixes {
+        println!("{:?}", pref);
+    }
+
+    let prefixes = c2.prefix2(); // veya prefixes()
+
+    for pref in prefixes {
+        println!("{:?}", pref);
+    }
+
+    let chars = MyCollection::VecVariant(vec!["1","2","3"]);
+    let cd = chars.joins("-");
+    println!("{:?}", cd)
+
 }
 
 fn max(list:&[i32])->(i32, &str){
 
-    let len = list.len();
+    let len = list.len();   
     let mut max = list[0];
 
     if len == 0 {
@@ -60,7 +92,7 @@ fn sum(list:&[i32])->(i32,&str){
 
 }
 
-fn prefix(list:&[i32])->Vec<Vec<i32>>{
+fn prefix1(list:&[i32])->Vec<Vec<i32>>{
 
 
     let len = list.len();
@@ -107,8 +139,6 @@ fn rotates(list:&[&str],n:usize)->Vec<String>{
 
 }
 
-// Write a function that converts a String of digits into a number. Tolerate whitespaces at the beginning and the end of the String.
-
 fn converts(a:String)->usize{
 
     let mut number = 0;
@@ -125,4 +155,138 @@ fn converts(a:String)->usize{
         }
     }
     number
+}
+#[derive(Debug)]
+enum MyCollection<T> {
+    VecVariant(Vec<T>),
+    ArrayVariant([T;10])
+}
+
+impl <T: PartialOrd+Copy+Clone+Display> MyCollection<T> {
+    fn find_max(&self)-> Option<T>{
+        match self{
+            MyCollection::VecVariant(v)=> {
+                if v.is_empty(){
+                    return None;
+                }
+
+                let mut max = v[0];
+                for i in 1..v.len(){
+                    if max < v[i] {
+                        max = v[i]
+                    }
+                }
+                Some(max)
+            }
+
+            MyCollection::ArrayVariant(a)=>{
+                if a.is_empty(){
+                    return None;
+                }
+
+                let mut max = a[0];
+                for i in 1..a.len(){
+                    if max < a[i] {
+                        max = a[i]
+                    }
+                }
+                Some(max)
+            }
+        }
+    }
+
+    fn rotate_left(&mut self, n:usize){
+        match self{
+            MyCollection::VecVariant(v) => {
+                let len = v.len();
+                if v.is_empty(){
+                    return;
+                }
+                let k = n % len;
+                
+                for _ in 0..k{
+                    let first = v[0];
+                    for i in 0..len-1{
+                        v[i] = v[i+1]
+                    }
+                    v[len-1] = first;
+                }
+            } 
+            MyCollection::ArrayVariant(a)=>{
+                let len = a.len();
+                if a.is_empty(){
+                    return;
+                }
+                let k = n % len;
+                
+                for _ in 0..k{
+                    let first = a[0];
+                    for i in 0..len-1{
+                        a[i]=a[i+1]
+                    }
+                    a[len-1] = first;
+                }
+            }
+        }
+    }
+    fn prefix2(&self)->Vec<Vec<T>>{
+        let mut result = Vec::new();
+        match self {
+            MyCollection::VecVariant(v)=> {
+                let len = v.len();
+
+                for i in 1..=len{
+                    let mut pref = Vec::new();
+
+                    for j in 0..i{
+                        pref.push(v[j]);
+                    }
+                    result.push(pref);
+                }
+                result
+            }
+
+            MyCollection::ArrayVariant(a)=> {
+                let len = a.len();
+
+                for i in 1..=len{
+                    let mut pref = Vec::new();
+
+                    for j in 0..i{
+                        pref.push(a[j]);
+                    }
+                    result.push(pref);
+                }
+                result
+            }
+        }
+    }
+
+    fn joins(&self, delimiter: &str) -> String {
+        let mut output = String::new();
+
+        match self {
+            MyCollection::VecVariant(v) => {
+                let mut i = 0;
+                while i < v.len() {
+                    output.push_str(&v[i].to_string());
+                    if i < v.len() - 1 {
+                        output.push_str(delimiter);
+                    }
+                    i += 1;
+                }
+            }
+            MyCollection::ArrayVariant(a) => {
+                let mut i = 0;
+                while i < a.len() {
+                    output.push_str(&a[i].to_string());
+                    if i < a.len() - 1 {
+                        output.push_str(delimiter);
+                    }
+                    i += 1;
+                }
+            }
+        }
+        output
+    }
 }
